3fbf3d3a9b398abba9ea0ed8513eb80dc4246a0b
juandiegombr <juandiegomartinblas@gmail.com>
2025-11-03 07:00:14 +0100

    Connect frontend with backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,21 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function App() {
+  const [message, setMessage] = useState("");
 
   useEffect(() => {
     fetch(import.meta.env.VITE_API_URL, { method: "GET", headers: { "Content-Type": "application/json" } })
       .then((response) => response.json())
-      .then((data) => console.log("Data from backend: ", data))
+      .then((data) => {
+        setMessage(data.data.message);
+      })
       .catch((error) => console.error("Error fetching: ", error));
   }, []);
 
   return (
     <div style={{ padding: 20 }}>
       <h1>🌾 Collitap</h1>
+      <p>{message}</p>
     </div>
   );
 }
